9311dd3c3e78274ed71e499c5c57eb64599a8360
Teodor Todorov <tt@versori.io>
2025-11-25 08:59:24 +0000

    view file test

--- pkg/sdk/tools/view_file_test.go
+++ pkg/sdk/tools/view_file_test.go
@@ -1,10 +1,73 @@
 package tools
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 )
+
+const viewFIleTestContent = `package tools
+
+import (
+	"fmt"
+	"time"
+)
+
+// TimeTool returns the current time
+type TimeTool struct{}
+
+func (t *TimeTool) Name() string {
+	return "time"
+}
+
+func (t *TimeTool) Description() string {
+	return "Returns the current date and time."
+}
+
+func (t *TimeTool) Init(config map[string]string) {
+}
+
+func (t *TimeTool) InputSchema() map[string]interface{} {
+	return map[string]interface{}{
+		"type": "object",
+		"properties": map[string]interface{}{
+			"format": map[string]interface{}{
+				"type":        "string",
+				"description": "Optional format. 'iso' for ISO8601, 'unix' for Unix timestamp. Default is human readable.",
+				"enum":        []string{"iso", "unix", "human"},
+			},
+		},
+	}
+}
+
+func (t *TimeTool) Call(input map[string]interface{}) (interface{}, error) {
+	now := time.Now()
+	format, _ := input["format"].(string)
+
+	switch format {
+	case "iso":
+		return now.Format(time.RFC3339), nil
+	case "unix":
+		return fmt.Sprintf("%d", now.Unix()), nil
+	default:
+		return now.Format("Monday, 02 Jan 2006 15:04:05 MST"), nil
+	}
+}
+`
+
+// careful with spaces and tabs when editing. It is hard to see when debugging
+const expectOutput = `<filePath>%s</filePath>
+<viewRange>5-10 of 48</viewRange>
+<content>
+     5  	"time"
+     6  )
+     7  
+     8  // TimeTool returns the current time
+     9  type TimeTool struct{}
+    10  
+</content>
+`
 
 func TestViewFile_readsFile(t *testing.T) {
 	viewTool := &ViewFileTool{}
@@ -16,12 +79,13 @@ func TestViewFile_readsFile(t *testing.T) {
 
 	path := filepath.Join(cwd, "testfiles", "view_file.txt")
 
-	if err := os.WriteFile(path, []byte(testContest), 0o600); err != nil {
+	if err := os.WriteFile(path, []byte(viewFIleTestContent), 0o600); err != nil {
 		t.Fatalf("failed to write test file: %v", err)
 	}
 
 	input := map[string]interface{}{
-		"path": path,
+		"path":       path,
+		"view_range": []int{5, 10},
 	}
 
 	result, err := viewTool.Call(input)
@@ -29,5 +93,7 @@ func TestViewFile_readsFile(t *testing.T) {
 		t.Fatalf("failed to view file: %v", err)
 	}
 
-	t.Log(result)
+	if result != fmt.Sprintf(expectOutput, path) {
+		t.Fatalf("expected %s, got %s", expectOutput, result)
+	}
 }
